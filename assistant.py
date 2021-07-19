04b01b7b445bc7348f376f6a3a913cbac8cdfaf0
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-19 15:55:15 +0530

    Initial Commit

--- assistant.py
+++ assistant.py
@@ -1,11 +1,12 @@
 from telethon import TelegramClient
 import os
 
+from sample_config import Config
 owner = os.environ.get("OWNER_ID", 1732236209)
 
-TOKEN = 
-API_ID = 
-API_HASH = 
+TOKEN = Config.TOKEN
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
 
 # kangers = os.environ.get("KANGERS_ID") 
 # For using the above var, first add "KANGERS_ID" var in app.json (https://github.com/MadBoy-X/SuperBot-Assistant/blob/main/app.json) !!
