6cc1d14a95ed2cd87b440c7e6592a0d3e6e2a729
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-19 13:43:24 +0530

    Initial Commit

---
+++ sample_config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config(object):
+    TOKEN = os.environ.get("TG_BOT_TOKEN", None)
+
+    API_ID = int(os.environ.get("APP_ID", None)
+
+    API_HASH = os.environ.get("API_HASH", None)
+                 
+    ENV = os.environ.get("ENV", "ANYTHING")
