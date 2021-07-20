a03207973401dd2108f97f4872d58a1343f7a853
Pranav <83628215+Pranav18262@users.noreply.github.com>
2021-07-20 11:58:46 +0530

    sayad ab var na bharna pade

--- sample_config.py
+++ sample_config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config(object):
-    TOKEN = "TOKEN"
+    TOKEN = "1879528628:AAG72fkdTwuJt3BQikiGuQZRkg7n4zDbJKg"
 
-    API_ID = 123456
+    API_ID = 1879263
 
-    API_HASH = "awoo"
+    API_HASH = "95ecb21d294c7f6ffdce81558d27999a"
                  
     ENV = "ANYTHING"
